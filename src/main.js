244ceecfef0637f22af52c17f7d296e597e2dceb
AJ Ancheta <ashleyjancheta@gmail.com>
2019-06-07 16:01:54 -0700

    fix spacing

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 
 $(document).ready(function () {
-	$("#search-name").submit(function(event) {
+	$("#search-name").submit(function (event) {
 		event.preventDefault();
 		$(".results").show();
 		let nameInput = $('#name').val();
@@ -18,33 +18,33 @@ $(document).ready(function () {
 			$("#newPt").append(newProvider.displayNewPatient(body));
 			$("#phone").append(newProvider.displayPhoneNumber(body));
 			$("#address").append(newProvider.displayAddress(body));
-			
-			
+
+
 		}, function (error) {
 			$('.showErrors').text(`There was an error processing your request: ${error.message}`);
 
 		});
 	});
 
-		$("#search-specialty").submit(function(event) {
-			event.preventDefault();
-			$(".results").show();
-			let practice = $('#name').val();
-			let newProvider = new Provider();
-			let promise = newProvider.searchByPractice(practice);
-	
-			promise.then(function (response) {
-				let body = JSON.parse(response);
-				$("#providerName").append(newProvider.displayName(body));
-				$("#newPt").append(newProvider.displayNewPatient(body));
-				$("#phone").append(newProvider.displayPhoneNumber(body));
-				$("#address").append(newProvider.displayAddress(body));
-				
-				
-			}, function (error) {
-				$('.showErrors').text(`There was an error processing your request: ${error.message}`);
-			});
-	
+	$("#search-specialty").submit(function (event) {
+		event.preventDefault();
+		$(".results").show();
+		let practice = $('#name').val();
+		let newProvider = new Provider();
+		let promise = newProvider.searchByPractice(practice);
+
+		promise.then(function (response) {
+			let body = JSON.parse(response);
+			$("#providerName").append(newProvider.displayName(body));
+			$("#newPt").append(newProvider.displayNewPatient(body));
+			$("#phone").append(newProvider.displayPhoneNumber(body));
+			$("#address").append(newProvider.displayAddress(body));
+
+
+		}, function (error) {
+			$('.showErrors').text(`There was an error processing           your request: ${error.message}`);
+		});
+
 	});
 });
 

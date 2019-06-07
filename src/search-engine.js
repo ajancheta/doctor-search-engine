a38f23a77ac770233a804f0a8af29a6c2355e24b
AJ Ancheta <ashleyjancheta@gmail.com>
2019-06-07 10:35:22 -0700

    add search by symptom call

---
+++ src/search-engine.js
@@ -0,0 +1,44 @@
+export default class Doctor {
+    constructor(nameInput, symptomInput) {
+        this.nameInput = name;
+        this.symptomInput = symptom;
+    }
+
+    searchByProvider() {
+      return new Promise(function (resolve, reject) {
+        let request = new XMLHttpRequest();
+        let location = "or-portland";
+        let key = process.env.API_KEY;
+        let url = `https://api.betterdoctor.com/2016-03-01/doctors?location=${location}&skip=0&limit=10&name=${this.nameInput}&user_key=${key}`;
+
+        request.onload = function () {
+          if (this.status === 200) {
+            resolve(request.response);
+          } else {
+             reject(Error(request.statusText));
+          }
+        }
+        request.open("GET", url, true);
+        request.send();
+      });
+    } 
+
+    searchBySymptom() {
+      return new Promise(function (resolve, reject) {
+        let request = new XMLHttpRequest();
+        let location = "or-portland";
+        let key = process.env.API_KEY;
+        let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${this.symptomInput}&location=${location}&skip=0&limit=10&user_key=${key}`;
+        
+        request.onload = function () {
+          if (this.status === 200) {
+            resolve(request.response);
+          } else {
+             reject(Error(request.statusText));
+          }
+        }
+        request.open("GET", url, true);
+        request.send();
+      });
+    } 
+  }

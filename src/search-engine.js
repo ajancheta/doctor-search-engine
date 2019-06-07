452ce0286869d7771a437888055f9af2f567409d
AJ Ancheta <ashleyjancheta@gmail.com>
2019-06-07 12:01:14 -0700

    remove constructor and pass in provider name // number of providers in function, printing info - in progress

--- src/search-engine.js
+++ src/search-engine.js
@@ -1,15 +1,13 @@
-export default class Provider {
-    constructor(nameInput, symptomInput) {
-        this.nameInput = name;
-        this.symptomInput = symptom;
-    }
+let apiKey = process.env.exports.API_KEY;
 
-    searchByProvider() {
+export default class Provider {
+
+    searchByProvider(providerName, numberOfProviders) {
       return new Promise(function (resolve, reject) {
         let request = new XMLHttpRequest();
         let location = "or-portland";
-        let key = process.env.API_KEY;
-        let url = `https://api.betterdoctor.com/2016-03-01/doctors?location=${location}&skip=0&limit=10&name=${this.nameInput}&user_key=${key}`;
+        
+        let url = `https://api.betterdoctor.com/2016-03-01/doctors?location=${location}&skip=0&limit=10&name=${this.nameInput}&user_key=${apiKey}`;
 
         request.onload = function () {
           if (this.status === 200) {
@@ -23,12 +21,13 @@ export default class Provider {
       });
     } 
 
-    searchBySymptom() {
+    searchByPractice() {
       return new Promise(function (resolve, reject) {
         let request = new XMLHttpRequest();
         let location = "or-portland";
         let key = process.env.API_KEY;
-        let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${this.symptomInput}&location=${location}&skip=0&limit=10&user_key=${key}`;
+        let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${practice}&location=${location}&skip=0&limit=${count}&user_key=${apiKey}`;
+
         
         request.onload = function () {
           if (this.status === 200) {
@@ -42,3 +41,27 @@ export default class Provider {
       });
     } 
   }
+
+  displayName(response){
+    let result = "";
+
+    if(response.data.length === 0){
+      return "There was no matching result";
+    }
+    for(let i = 0; i < response.data.length; i++) {
+      result += '<li>' + response.data[i].profile.first_name + " " + response.data[i].profile.last_name + '</li>';
+    }
+    return result;
+  }
+
+  displayAddress(response){
+    let result = "";
+
+    for(let i = 0; i < response.data.length; i++) {
+      result += '<li>' + response.data[i].practices[0].visit_address.street + " " + response.data[i].practices[0].visit_address.city + " " + response.data[i].practices[0].visit_address.state + " " + response.data[i].practices[0].visit_address.zip + "</li><br>";
+
+    }
+    return result;
+  }
+
+  

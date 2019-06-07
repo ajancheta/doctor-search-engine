646f51a97037f8b16c3fda8c61e30d7381bafe3a
AJ Ancheta <ashleyjancheta@gmail.com>
2019-06-07 14:49:41 -0700

    store user input and create an object of info - in progress

--- src/main.js
+++ src/main.js
@@ -4,25 +4,28 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 
-$(document).ready(function() {
-    
-    $("#search-name").submit(function(event) {  
-			event.preventDefault();
-			let nameInput = $('#nameInput').val();
-			let newProvider = new Provider();
-			let promise = newProvider.searchByProvider(name);
-	
-      promise.then(function(response) {
-				let body = JSON.parse(response);
-				
-				$("#name").append(Provider.displayName(body));
-				$("#newPatient").append(Provider.displayNewPatient(body));
-				$("#phone").append(Provider.displayPhone(body));
-				$("#address").append(Provider.displayAddress(body));
-				$("#website").append(Provider.displayWebsite(body));
-        
-      }, function(error) {
-        $('.showErrors').text(`There was an error processing your request: ${error.message}`);
-      });
-    });
-  });
+$(document).ready(function () {
+	$("#search-name").submit(function(event) {
+		event.preventDefault();
+		$(".results").show();
+		let nameInput = $('#nameInput').val();
+		let newProvider = new Provider();
+		let promise = newProvider.searchByProvider(name);
+		console.log(newProvider);
+		console.log(nameInput);
+		promise.then(function (response) {
+			let body = JSON.parse(response);
+			$("#name").append(newProvider.displayName(body));
+			$("#newPt").append(newProvider.displayNewPatient(body));
+			$("#phone").append(newProvider.displayPhone(body));
+			$("#address").append(newProvider.displayAddress(body));
+			$("#website").append(newProvider.displayWebsite(body));
+			
+			
+		}, function (error) {
+			$('.showErrors').text(`There was an error processing your request: ${error.message}`);
+		});
+	});
+	console.log("test");
+});
+
